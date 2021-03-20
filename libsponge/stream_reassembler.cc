e527ab8cbccb3bca200816ec4e2bb773330ce224
FlyingPig <1800012735@pku.edu.cn>
2021-03-21 01:34:10 +0800

    lab1: stream assembler finished

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -12,15 +12,72 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity) : unass_base(0), unass_size(0), _eof(0), buffer(capacity, '\0'), bitmap(capacity, false), _output(capacity), _capacity(capacity) {}
+
+//! \details This functions calls just after pushing a substring into the 
+//! _output stream. It aims to check if there exists any contiguous substrings
+//! recorded earlier can be push into the stream.
+void StreamReassembler::check_contiguous() {
+    string tmp = "";
+    while (bitmap.front()) {
+        //cout<<"check one more contiguous substring"<<endl;
+        tmp += buffer.front();
+        buffer.pop_front();
+        bitmap.pop_front();
+        buffer.push_back('\0');
+        bitmap.push_back(false);
+    }
+    if (tmp.length() > 0) {
+        cout << "push one contiguous substring with length " << tmp.length() << endl;
+        _output.write(tmp);
+        unass_base += tmp.length();
+        unass_size -= tmp.length();
+    } 
+}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    if (eof) {
+        _eof = true;
+    }
+    size_t len = data.length();
+    if (index >= unass_base) {
+        int offset = index - unass_base;
+        size_t real_len = min(len, _capacity - _output.buffer_size() - offset);
+        if (real_len < len) {
+            _eof = false;
+        }
+        for (size_t i = 0; i < real_len; i++) {
+            if (bitmap[i + offset]) continue;
+            buffer[i + offset] = data[i];
+            bitmap[i + offset] = true;
+            unass_size++;
+        }
+    } else if (index + len > unass_base) {
+        int offset = unass_base - index;
+        size_t real_len = min(len - offset, _capacity - _output.buffer_size());
+        if (real_len < len - offset) {
+            _eof = false;
+        }
+        for (size_t i = 0; i < real_len; i++) {
+            if (bitmap[i]) continue;
+            buffer[i] = data[i + offset];
+            bitmap[i] = true;
+            unass_size++;
+        }
+    }
+    check_contiguous();
+    if (_eof && unass_size == 0) {
+        _output.end_input();
+    }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const {
+    return unass_size;
+}
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const {
+    return unass_size == 0;
+}
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,15 +5,26 @@
 
 #include <cstdint>
 #include <string>
+#include <deque>
+#include <iostream>
+#include <algorithm>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
 class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
+    size_t unass_base;   //!< The index of the first unassembled byte
+    size_t unass_size;   //!< The number of bytes in the substrings stored but not yet reassembled 
+    bool _eof;           //!< The last byte has arrived
+    std::deque<char> buffer; //!< The unassembled strings
+    std::deque<bool> bitmap; //!< buffer bitmap
 
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
+
+    void check_contiguous();
+    size_t real_size(const std::string &data, const size_t index);
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
