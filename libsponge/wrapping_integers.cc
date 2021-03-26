000e74c33e3ef3856f46507324985d6824db40f2
FlyingPig <60659728+PKUFlyingPig@users.noreply.github.com>
2021-03-26 18:04:20 +0800

    wrapping_integers finished

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,5 +1,5 @@
 #include "wrapping_integers.hh"
-
+#include <iostream>
 // Dummy implementation of a 32-bit wrapping integer
 
 // For Lab 2, please replace with a real implementation that passes the
@@ -14,8 +14,7 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    return isn + uint32_t(n);
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +28,16 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint64_t tmp = 0;
+    uint64_t tmp1 = 0;
+    if (n - isn < 0) {
+        tmp = uint64_t(n - isn + (1l<<32));
+    } else {
+        tmp = uint64_t(n - isn); 
+    }
+    if (tmp >= checkpoint) return tmp;
+    tmp |= ((checkpoint >> 32) << 32);
+    while (tmp <= checkpoint) tmp += (1ll << 32);
+    tmp1 = tmp - (1ll << 32); 
+    return (checkpoint - tmp1 < tmp - checkpoint)? tmp1 : tmp;
 }
